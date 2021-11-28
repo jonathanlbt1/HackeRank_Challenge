30d3ee9d9c8c5fd85c760e8fdc68c86e95e1b383
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2021-11-27 21:57:30 -0300

    'creating first file'

---
+++ Day_00/challenge_0.py
@@ -0,0 +1,8 @@
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input('Input your string here: ') #Welcome to 30 Days of Code!
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+print(input_string)
